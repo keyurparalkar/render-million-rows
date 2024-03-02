d734f6ac2686b54e283b98ca9c143dbba18c3489
Keyur Paralkar <keyurparalkar@Keyurs-MBP.bbrouter>
2024-03-02 22:11:36 +0530

    feat: initialized a vite ts project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>A million row challenge</h1>
+			<canvas
+				id="table"
+				style={{ border: "1px solid white" }}
+				width="800px"
+				height="500px"
+			></canvas>
+		</>
+	);
+}
+
+export default App;
